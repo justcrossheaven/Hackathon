7d03fcaf4d0e5dc798ecd2ccd52aaba11483061f
john <63093431+justcrossheaven@users.noreply.github.com>
2022-07-24 09:40:56 +1200

    Texteditor (#1)
    * texteditor
    
    * refactor
    
    * text page styled
    
    * refactor
    
    * update string manipulation
    
    * added grammarly
    
    * merged pet canvas in
    
    * Remove unnecessary orbit control
    
    * add in sleep motion and lock buttons
    
    * Implement age unlock system by words
    
    Co-authored-by: Jason <pwan365@aucklanduni.ac.nz>
    Co-authored-by: myao209 <63892309+Hakusen-git@users.noreply.github.com>

--- packages/frontend/src/Components/GameCanvas/GameCanvas.js
+++ packages/frontend/src/Components/GameCanvas/GameCanvas.js
@@ -1,27 +1,113 @@
-import React from "react";
-import pic from "../../img/cat_idle.gif";
-import GameButtons from "../GameButtons/GameButtons";
+import React, { Suspense, useEffect, useState } from "react";
+import pic from "../../img/gameCanvas.jpeg";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import { Html } from "@react-three/drei";
+import { Button } from "@mui/material";
+import catIdle from "../../img/cat_sit.gif";
+import catEat from "../../img/cat_eat.gif";
+import catNap from "../../img/cat_nap.gif";
 
-const GameCanvas = () => {
+function Image() {
+  const texture = useLoader(TextureLoader, pic);
   return (
-    <div>
-      <div className="game-canvas-parent">
-        <div className="game-canvas-child"></div>
-        <GameButtons />
-        <img
-          src={pic}
-          style={{
-            zIndex: "100",
-            position: "absolute",
-            left: "45%",
-            bottom: "18%",
-            width: "300px",
-            height: "300px",
-            marginTop: "-150px",
-            marginLeft: "-150px",
-          }}
-        />
-      </div>
+    <mesh position={[0, 0, 3.2]}>
+      <planeBufferGeometry attach="geometry" args={[3, 3]} />
+      <meshBasicMaterial attach="material" map={texture} />
+    </mesh>
+  );
+}
+
+const buttonStyle = {
+  borderRadius: "5px",
+  padding: "18px",
+  margin: "0",
+  fontSize: "2rem",
+  width: "160px",
+};
+
+const GameCanvas = (props) => {
+  const [catStatus, setCatStatus] = useState(catIdle);
+  const [catAge, setCatAge] = useState(-1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCatStatus(catNap);
+    }, 10000);
+  }, []);
+  console.log(catAge);
+  useEffect(() => {
+    if (props.wordCount % 10 === 0) {
+      setCatAge(catAge + 1);
+    }
+  }, [props.wordCount]);
+
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <Canvas colorManagement>
+        <Suspense fallback={null}>
+          <Image />
+        </Suspense>
+        <group position={[0, 0, 0]} rotation={[0, 0, 0]}>
+          <Html center distanceFactor={3} position={[-1.1, 0, 0]}>
+            <div style={{ position: "absolute" }}>
+              <img
+                src={catStatus}
+                style={{
+                  width: "500px",
+                  height: "500px",
+                }}
+              />
+            </div>
+          </Html>
+        </group>
+        <Html center distanceFactor={3} position={[-1, 0, 0]}>
+          <div>
+            <Button
+              variant="contained"
+              sx={buttonStyle}
+              onClick={() => {
+                setCatStatus(catEat);
+                setTimeout(() => {
+                  setCatStatus(catIdle);
+                  setTimeout(() => {
+                    setCatStatus(catNap);
+                  }, 10000);
+                }, 4000);
+              }}
+              disableElevation
+              disabled={catAge > 0 ? false : true}
+            >
+              Feed
+            </Button>
+          </div>
+        </Html>
+        <Html center distanceFactor={3} position={[0, 1, 0]}>
+          <div>
+            <Button
+              variant="contained"
+              sx={buttonStyle}
+              disableElevation
+              disabled={catAge > 1 ? false : true}
+            >
+              Litter
+            </Button>
+          </div>
+        </Html>
+        <Html center distanceFactor={3} position={[1, 0, 0]}>
+          <div>
+            <Button
+              variant="contained"
+              sx={buttonStyle}
+              disableElevation
+              disabled={catAge > 2 ? false : true}
+            >
+              Water
+            </Button>
+          </div>
+        </Html>
+      </Canvas>
     </div>
   );
 };
---
+++ packages/frontend/src/components/SlateEditor/Editor.jsx
@@ -0,0 +1,229 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { withHistory } from "slate-history";
+import { Slate, Editable, withReact } from "slate-react";
+import Toolbar from "./Toolbar/Toolbar";
+import { sizeMap, fontFamilyMap } from "./utils/SlateUtilityFunctions.js";
+import withLinks from "./plugins/withLinks.js";
+import withTables from "./plugins/withTable.js";
+import withEmbeds from "./plugins/withEmbeds.js";
+import "./Editor.css";
+import Link from "./Elements/Link/Link";
+import Image from "./Elements/Image/Image";
+import Video from "./Elements/Video/Video";
+
+import { GrammarlyEditable, GrammarlySlate } from "@grammarly/react-slate";
+import { Typography } from "@mui/material";
+
+const Element = (props) => {
+  const { attributes, children, element } = props;
+
+  switch (element.type) {
+    case "headingOne":
+      return <h1 {...attributes}>{children}</h1>;
+    case "headingTwo":
+      return <h2 {...attributes}>{children}</h2>;
+    case "headingThree":
+      return <h3 {...attributes}>{children}</h3>;
+    case "blockquote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "alignLeft":
+      return (
+        <div
+          style={{ textAlign: "left", listStylePosition: "inside" }}
+          {...attributes}
+        >
+          {children}
+        </div>
+      );
+    case "alignCenter":
+      return (
+        <div
+          style={{ textAlign: "center", listStylePosition: "inside" }}
+          {...attributes}
+        >
+          {children}
+        </div>
+      );
+    case "alignRight":
+      return (
+        <div
+          style={{ textAlign: "right", listStylePosition: "inside" }}
+          {...attributes}
+        >
+          {children}
+        </div>
+      );
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "orderedList":
+      return (
+        <ol type="1" {...attributes}>
+          {children}
+        </ol>
+      );
+    case "unorderedList":
+      return <ul {...attributes}>{children}</ul>;
+    case "link":
+      return <Link {...props} />;
+
+    case "table":
+      return (
+        <table>
+          <tbody {...attributes}>{children}</tbody>
+        </table>
+      );
+    case "table-row":
+      return <tr {...attributes}>{children}</tr>;
+    case "table-cell":
+      return <td {...attributes}>{children}</td>;
+    case "image":
+      return <Image {...props} />;
+    case "video":
+      return <Video {...props} />;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+  if (leaf.strikethrough) {
+    children = (
+      <span style={{ textDecoration: "line-through" }}>{children}</span>
+    );
+  }
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+  if (leaf.superscript) {
+    children = <sup>{children}</sup>;
+  }
+  if (leaf.subscript) {
+    children = <sub>{children}</sub>;
+  }
+  if (leaf.color) {
+    children = <span style={{ color: leaf.color }}>{children}</span>;
+  }
+  if (leaf.bgColor) {
+    children = (
+      <span style={{ backgroundColor: leaf.bgColor }}>{children}</span>
+    );
+  }
+  if (leaf.fontSize) {
+    const size = sizeMap[leaf.fontSize];
+    children = <span style={{ fontSize: size }}>{children}</span>;
+  }
+  if (leaf.fontFamily) {
+    const family = fontFamilyMap[leaf.fontFamily];
+    children = <span style={{ fontFamily: family }}>{children}</span>;
+  }
+  return <span {...attributes}>{children}</span>;
+};
+const SlateEditor = (props) => {
+  const editor = useMemo(
+    () =>
+      withHistory(withEmbeds(withTables(withLinks(withReact(createEditor()))))),
+    []
+  );
+
+  const [value, setValue] = useState([
+    {
+      type: "paragaph",
+      children: [{ text: "First line of text in Slate JS. " }],
+    },
+  ]);
+
+  const onChange = useCallback(
+    (value) => {
+      setValue(value);
+      const content = JSON.stringify(value);
+    },
+    [setValue]
+  );
+
+  const wordCount = () => {
+    let count = 0;
+    value.forEach((element) => {
+      element.children.forEach((child) => {
+        if (child.text) {
+          let s = child.text;
+          s = s.replace(/\s\s+/g, " ");
+          s = s.replace(/  +/g, " ");
+          count += s.split(" ").length;
+        } else if (child.children) {
+          child.children.forEach((child) => {
+            if (child.text) {
+              let s = child.text;
+              s = s.replace(/\s\s+/g, " ");
+              s = s.replace(/  +/g, " ");
+              count += s.split(" ").length;
+            }
+          });
+        }
+      });
+    });
+    props.setWordCount(count);
+    return count;
+  };
+
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
+
+  return (
+    <div>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      >
+        <Toolbar />
+        <div
+          className="editor-wrapper"
+          style={{
+            border: "1px solid #f3f3f3",
+            padding: "0 10px",
+          }}
+        >
+          {/* <Editable
+                        placeholder='Write something'
+                        renderElement={renderElement} 
+                        renderLeaf={renderLeaf}
+                    /> */}
+          <GrammarlySlate value={value} clientId="your-client-id-here">
+            <GrammarlyEditable
+              placeholder="Write something"
+              renderElement={renderElement}
+              renderLeaf={renderLeaf}
+            />
+          </GrammarlySlate>
+        </div>
+      </Slate>
+      <Typography
+        style={{
+          position: "absolute",
+          bottom: "10px",
+          right: "20px",
+          color: "grey",
+          pointerEvents: "none",
+        }}
+      >
+        Next Level: {wordCount() - 1}/100
+      </Typography>
+    </div>
+  );
+};
+
+export default SlateEditor;
