5cbff11997163ad243554337d7a26c16094c6c66
myao209 <63892309+Hakusen-git@users.noreply.github.com>
2022-07-24 01:07:54 +1200

    merged pet canvas in

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,17 +1,17 @@
-import './stylesheets/App.css';
-import SlateEditor from './components/SlateEditor/Editor';
+import "./stylesheets/App.css";
+import SlateEditor from "./components/SlateEditor/Editor";
+import GameCanvas from "./components/GameCanvas/GameCanvas";
 function App() {
   return (
     <div className="App">
       <div className="text-page">
-        <SlateEditor/>
-      </div>    
+        <SlateEditor />
+      </div>
       <div className="test">
-        <p>placeholder</p>
-      </div>     
+        <GameCanvas />
+      </div>
     </div>
   );
 }
 
 export default App;
-
--- packages/frontend/src/Components/GameCanvas/GameCanvas.js
+++ packages/frontend/src/Components/GameCanvas/GameCanvas.js
@@ -1,25 +1,86 @@
-import React from "react";
-import pic from "../../img/cat_idle.gif";
+import React, { Suspense, useState } from "react";
+import pic from "../../img/gameCanvas.jpeg";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import { Html, OrbitControls } from "@react-three/drei";
+import { Button } from "@mui/material";
+import catIdle from "../../img/cat_sit.gif";
+import catEat from "../../img/cat_eat.gif";
+
+function Image() {
+  const texture = useLoader(TextureLoader, pic);
+  return (
+    <mesh position={[0, 0, 3.2]}>
+      <planeBufferGeometry attach="geometry" args={[3, 3]} />
+      <meshBasicMaterial attach="material" map={texture} />
+    </mesh>
+  );
+}
+
+const buttonStyle = {
+  borderRadius: "5px",
+  padding: "18px",
+  margin: "0",
+  fontSize: "2rem",
+  width: "160px",
+};
 
 const GameCanvas = () => {
+  const [catStatus, setCatStatus] = useState(catIdle);
+
   return (
-    <div>
-      <div className="game-canvas-parent" style={{ zIndex: "-1" }}>
-        <div className="game-canvas-child"></div>
-        <img
-          src={pic}
-          style={{
-            zIndex: "100",
-            position: "absolute",
-            left: "45%",
-            bottom: "18%",
-            width: "300px",
-            height: "300px",
-            marginTop: "-150px",
-            marginLeft: "-150px",
-          }}
-        />
-      </div>
+    <div style={{ height: "100%", width: "100%" }}>
+      <Canvas colorManagement>
+        <Suspense fallback={null}>
+          <Image />
+        </Suspense>
+        <group position={[0, 0, 0]} rotation={[0, 0, 0]}>
+          <Html center distanceFactor={3} position={[-1.1, 0, 0]}>
+            <div style={{ position: "absolute" }}>
+              <img
+                src={catStatus}
+                style={{
+                  width: "500px",
+                  height: "500px",
+                }}
+              />
+            </div>
+          </Html>
+        </group>
+        <Html center distanceFactor={3} position={[-1, 0, 0]}>
+          <div>
+            <Button
+              variant="contained"
+              sx={buttonStyle}
+              onClick={() => {
+                setCatStatus(catEat);
+                setTimeout(() => {
+                  setCatStatus(catIdle);
+                }, 4000);
+              }}
+              disableElevation
+            >
+              Feed
+            </Button>
+          </div>
+        </Html>
+        <Html center distanceFactor={3} position={[0, 1, 0]}>
+          <div>
+            <Button variant="contained" sx={buttonStyle} disableElevation>
+              Litter
+            </Button>
+          </div>
+        </Html>
+        <Html center distanceFactor={3} position={[1, 0, 0]}>
+          <div>
+            <Button variant="contained" sx={buttonStyle} disableElevation>
+              Water
+            </Button>
+          </div>
+        </Html>
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 };
