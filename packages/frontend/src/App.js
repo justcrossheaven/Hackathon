d96c7c1bb4bb8aacb6f9556f648289cd14128f3c
Jason <pwan365@aucklanduni.ac.nz>
2022-07-24 11:25:04 +1200

    routed HomePage

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./stylesheets/App.css";
+import HomePage from "./components/HomePage/HomePage";
 import EditPage from "./components/EditPage/EditPage";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
@@ -11,8 +12,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<EditPage/>} />
-          {/* <Route exact path="/home" element={<HomePage/>} /> */}
+          <Route exact path="/" element={<HomePage/>} />
+          <Route exact path="/edit" element={<EditPage/>} />
           {/* <Route exact path="/list" element={<ListPage/>} /> */}
           <Route path="*" element={<p>404 not found</p>} />  
         </Routes>   
