a8f9c3b0e5258bdd876a7fb4bffc42caef39224a
Jasmine Chen <jasminechen0621@gmail.com>
2022-07-23 19:54:07 +1200

    Update api

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -1,22 +1,47 @@
 const express = require('express');
+const User = require('./db');
 
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+app.get('/documentInfo', function (req, res) {
+    const dbDocument = await Document.findById(req.query.id);
+    
+    if (dbDocument) {
+        res.json(dbDocument);
+    } else {
+        res.statusCode(404);
+    }
 })
 
-app.get('/', (req, res) => {
+app.get('/alldocuments', (req, res) => {
+
   return res.send('Received a GET HTTP method');
 });
 
+//if loggin successfully, create local user
 app.post('/user', (req, res) => {
-  const emailExist = await User.findOne({ email: req.body.Id });
-  return res.send('Received a POST HTTP method');
+    const userExist = await User.findOne({ uid: req.body.uid });
+    
+    if (!userExist) {
+    const user = new User({
+        uid: req.body.uid,
+        name: req.body.name,
+        profilePic: req.body.profilePic
+    });
+    try {
+        await user.save();
+    } catch { err =>
+        res.status(400).send(err);
+    }
+  }
+  return res.send(200);
 });
 
+//update document
 app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+    
+
+    return res.send('Received a PUT HTTP method');
 });
 
 app.delete('/', (req, res) => {
