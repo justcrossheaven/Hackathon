504f75151557a964580b7a7dddaefbffc2b70374
myao209 <63892309+Hakusen-git@users.noreply.github.com>
2022-07-24 12:51:14 +1200

    emerge google login

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -4,34 +4,36 @@ import EditPage from "./components/EditPage/EditPage";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import { signInWithGoogle, registerWithEmailAndPassword, signInWithEmail } from './Firebase'; 
-import { userContext } from './userContext';
+import {
+  signInWithGoogle,
+  registerWithEmailAndPassword,
+  signInWithEmail,
+} from "./Firebase";
+import { userContext } from "./userContext";
+import React, { useState } from "react";
 
 function App() {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [userId, setUserId] = useState("");
 
   return (
-      <userContext.Provider value={{ userId, setUserId }}>
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<HomePage/>} />
-          <Route exact path="/edit" element={<EditPage/>} />
-          {/* <Route exact path="/list" element={<ListPage/>} /> */}
-          <Route exact path="/list" element={<p>list page</p>} />
-          <Route path="*" element={<p>404 not found</p>} />  
-        </Routes>   
-      </BrowserRouter>      
-      <button onClick={signInWithGoogle} className="login-with-google-btn">
-            Sign In With Google
-          </button>
-    </div>
-    
+    <userContext.Provider value={{ userId, setUserId }}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/edit" element={<EditPage />} />
+            {/* <Route exact path="/list" element={<ListPage/>} /> */}
+            <Route exact path="/list" element={<p>list page</p>} />
+            <Route path="*" element={<p>404 not found</p>} />
+          </Routes>
+        </BrowserRouter>
+        <button onClick={signInWithGoogle} className="login-with-google-btn">
+          Sign In With Google
+        </button>
+      </div>
     </userContext.Provider>
-
   );
 }
 
