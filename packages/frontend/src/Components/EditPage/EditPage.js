8913225422ff85bc0ee4f91530deb4cf09b950db
Jason <pwan365@aucklanduni.ac.nz>
2022-07-24 13:39:21 +1200

    added state in edit for loading doc

--- packages/frontend/src/Components/EditPage/EditPage.js
+++ packages/frontend/src/Components/EditPage/EditPage.js
@@ -3,10 +3,18 @@ import SlateEditor from "../SlateEditor/Editor";
 import GameCanvas from "../GameCanvas/GameCanvas";
 import { useState } from "react";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 function EditPage() {
   const [wordCount, setWordCount] = useState(0);
   let navigate = useNavigate();
+  const { state } = useLocation();
+//   const { title, document } = state;
+  let editorContent = [
+    {
+      type: "paragaph",
+      children: [{ text: "First line of text in Slate JS. " }],
+    },
+  ];
 
   const goBack = () => {
     navigate(`/list`);
@@ -19,7 +27,10 @@ function EditPage() {
             <div className="back-icon"><ArrowBackIcon fontSize="large" onClick={goBack}/></div>
             <h1>Title</h1>
         </div>
-        <div className="editor-box"><SlateEditor setWordCount={setWordCount} /></div>
+        <div className="editor-box">
+            {/* <SlateEditor setWordCount={setWordCount} /> */}
+            <SlateEditor setWordCount={setWordCount} editorContent={editorContent}/>
+        </div>
       </div>
       <div className="test">
         <GameCanvas wordCount={wordCount} />
