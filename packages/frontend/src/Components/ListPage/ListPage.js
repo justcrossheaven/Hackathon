f8672b0e32d168f18634cc121eb7fda980a967fb
myao209 <63892309+Hakusen-git@users.noreply.github.com>
2022-07-24 15:34:04 +1200

    Update ListPage.js

--- packages/frontend/src/Components/ListPage/ListPage.js
+++ packages/frontend/src/Components/ListPage/ListPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Document from "../Document/Document";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function ListPage() {
   const [documents, setDocuments] = useState([]);
 
